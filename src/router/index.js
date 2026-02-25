aaa3c1ef23bea17bb3244405bbb216c9b9c5c3bf
liaozk <liaozikai@beyhui.com>
2026-02-25 22:43:34 +0800

    Update Vite configuration and add new route for Test component

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/HelloWorld.vue'
+import Test from '../components/Test.vue'
 
 // 按需导入你的页面组件
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/test', component: Test },
   // 可添加更多路由
 ]
 
